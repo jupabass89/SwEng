b1a7473138486f13d513be142de31406f0577c00
juangome <jupabass89@gmail.com>
2020-11-12 01:47:38 -0500

    fixing paths

--- tarea-4-node/customerservice/modules/mongodb/mongodb.util.js
+++ tarea-4-node/customerservice/modules/mongodb/mongodb.util.js
@@ -7,7 +7,7 @@
 
     var mongoose = require('mongoose');
 
-    var mongodbConfig = require('../../config/mongodb/mongodb-config').mongodb;
+    var mongodbConfig = require('../../config/mongodb/mongodb-config.json').mongodb;
 
     function init() {
         var options = {
